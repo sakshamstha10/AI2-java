73dc766cb5175a3f3d8f338b18f45950a3fee37d
Saksham Shrestha <saksham10stha@gmail.com>
2025-11-20 13:39:20 +0545

    Date Conversion

---
+++ Week3/DateConversion.java
@@ -0,0 +1,31 @@
+package Week3;
+import java.util.Scanner;
+
+
+/**
+ * Write a description of class DateConversion here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DateConversion
+{
+   public static void main(String[]arg)
+   {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter Days:");
+       int TD = sc.nextInt();
+       int Years = TD/365;
+       int RD = TD%365;
+       
+       int Months = RD/30;
+       int Days = RD%30;
+       
+       System.out.println("Year: " + Years);
+        System.out.println("Months: " + Months);
+        System.out.println("Days: " + Days);
+       
+       
+   }
+   
+}
